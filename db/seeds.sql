ebb51fb22e4132b8b2f1ea13d137ac49634eabbf
William Harrison <alex@grahamsg.com>
2019-02-05 14:34:04 -0500

    updating homework

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Small Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Medium Mac', true);
